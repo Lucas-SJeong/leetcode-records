13a6400c145aec3699ee273347372738ed614e9d
Lucas-SJeong <lucas.s.jeong@gmail.com>
2026-08-11 23:55:42 +0900

    Time: 15 ms (75.47%), Space: 12.5 MB (16.47%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,7 +1,14 @@
 class Solution(object):
     def fib(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        
+        memo = {}
+        return self.memoization(n,memo)
+    def memoization(self,n,memo):
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+
+        if n not in memo:
+            memo[n] = self.memoization(n-1,memo) + self.memoization(n-2,memo)
+
+        return memo[n]
